4b186163634f05cf638dd7c3e2d839bb9777ae01
Brendan Lim <brendan.james.lim@gmail.com>
2024-09-21 10:50:52 -0700

    added files

---
+++ index.js
@@ -0,0 +1,57 @@
+const display = document.getElementById("display");
+let timer = null;
+let countdown = 50*1000*60;
+let startTime = Date.now() + countdown;
+let elapsedTime = countdown;
+let isRunning = false;
+
+
+function start() {
+    if(isRunning) {
+        clearInterval(timer);
+        elapsedTime = startTime - Date.now();
+        isRunning = false;
+        startBtn.textContent = `Start`;
+    }
+    else {
+        startTime = Date.now() + elapsedTime;
+        timer = setInterval(update, 1000);
+        isRunning = true;
+        startBtn.textContent = `Stop`;
+    }
+  
+}
+
+function reset() {
+    clearInterval(timer);
+    countdown= 50*1000*60;
+    elapsedTime = countdown;
+    isRunning = false;
+    startTime = Date.now() + countdown;
+
+    hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    seconds = Math.floor(elapsedTime / 1000 % 60);
+    display.textContent = `${minutes}:${seconds}0`;
+    startBtn.textContent = `Start`;
+}
+
+function update() {
+    
+    const currentTime = Date.now();
+    elapsedTime = startTime - currentTime;
+
+    if (elapsedTime <=0 ) {
+        clearInterval(timer);
+        display.textContent = "00:00";
+        startBtn.textContent = `Start`;
+        isRunning = false;
+        return;
+    }
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+
+    display.textContent = `${minutes}:${seconds}`;
+}
